837fb9fbd0a82f6c5531c3fe39d93a115799c973
Isaac Obradors <isaac.obradors@gmail.com>
2014-10-24 18:41:57 +0200

    First commit

---
+++ src/com/inteam/estrellawallet/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.inteam.estrellawallet;
+
+import com.inteam.androidwear.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second_activity);
+	}
+}
